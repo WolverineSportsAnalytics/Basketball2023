179b804c2bccf8726236a4a4f05d3cc1f8e060d2
jwkenned <123987530+jwkenned@users.noreply.github.com>
2023-02-06 20:50:56 -0500

    Add files via upload

--- basketball_reference.py
+++ basketball_reference.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 31 11:35:51 2023
+Created on Mon Feb  6 20:20:33 2023
 
-@author: kushal
+@author: jwken
 """
 
 import requests
@@ -21,25 +20,28 @@ if __name__ == "__main__":
         teamRows = teamStatTable.find_all("tr")
         
         
-        for row in teamRows[:20]:
+        for row in teamRows[:399]:
             
-           columns = row.find_all("td")
-           name = columns[0].text
-           wins = int (columns[2].text)
-           losses = int (columns[3].text)
-          #SOS = int (columns[6].text)
-           ptsFor = int (columns[17].text)
-           ptsAllowed = int (columns[18].text)
+            if((row.has_attr("class"))):
+                x = 4
+            else:
+                
+                columns = row.find_all("td")
+                name = columns[0].text
+                wins = int (columns[2].text)
+                losses = int (columns[3].text)
+               #SOS = int (columns[6].text)
+                ptsFor = int (columns[17].text)
+                ptsAllowed = int (columns[18].text)
+                three_pct = float (columns[26].text)
+                offensive_rebounds = int (columns[30].text)
+                turnovers = int (columns[35].text)
+                
+            
+                stats = [name, wins, losses, ptsFor, ptsAllowed, three_pct, turnovers, offensive_rebounds]
+                print (stats)
+                
+            
+         
            
            
-           stats = [name, wins, losses, ptsFor, ptsAllowed]
-      
-           print (stats)
-      
-      
-           
-      
-        
-      
-        
-      
