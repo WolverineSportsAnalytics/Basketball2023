fe71193026b81d0a42fb2e1f16888d6c0248ffdb
kushal5294 <113399080+kushal5294@users.noreply.github.com>
2023-02-06 20:23:36 -0500

    Add files via upload

---
+++ basketball_reference.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 11:35:51 2023
+
+@author: kushal
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+        
+        url = requests.get ("https://www.sports-reference.com/cbb/seasons/2023-school-stats.html")
+        
+        soup = BeautifulSoup(url.text, 'html.parser')
+        
+
+        teamStatTable = soup.find("div", attrs = {'id':'div_basic_school_stats'}).find("table").find("tbody")
+        
+        teamRows = teamStatTable.find_all("tr")
+        
+        
+        for row in teamRows[:20]:
+            
+           columns = row.find_all("td")
+           name = columns[0].text
+           wins = int (columns[2].text)
+           losses = int (columns[3].text)
+          #SOS = int (columns[6].text)
+           ptsFor = int (columns[17].text)
+           ptsAllowed = int (columns[18].text)
+           
+           
+           stats = [name, wins, losses, ptsFor, ptsAllowed]
+      
+           print (stats)
+      
+      
+           
+      
+        
+      
+        
+      
